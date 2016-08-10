911ff796eb184b5c1988edba1a1d36ad800b2dcc
Shirish Dali <designsbydali@gmail.com>
2016-08-10 16:13:57 -0500

    got the firstname and lastname working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-
-
-
+  //created input for first and last name
+  $('body').prepend('<input id="firstname" placeholder="Enter First Name">')
+  $('#firstname').after('<input id="lastname" placeholder="Enter Last Name">')
 
   //on page load populate featured-joke
   var api = 'http://api.icndb.com/jokes/random';
@@ -15,23 +15,32 @@ $(document).ready(function() {
 
   //onClick the get jokes button
   $('#btn-getjokes').on('click', function() {
-    // console.log('it worked');
-    var api = 'http://api.icndb.com/jokes/random';
+      // console.log('it worked');
+      var api = 'http://api.icndb.com/jokes/random';
+      var firstname = $('#firstname').val();
+      var lastname = $('#lastname').val();
 
-    $.ajax({
-        url: api
-      })
-      .done(function(data) {
-        console.log(data);
-        pushText();
-        $('#featured-joke').text(data.value.joke)
-      })
+      $.ajax({
+          url: api,
+          data: {
+            firstName: firstname,
+            lastName: lastname
 
-    function pushText() {
-      var joke = $('#featured-joke').text()
-      var li = $('<li></li>')
-      li.text(joke)
-      $('#previous-joke').append(li)
+          }
+        })
+        .done(function(data) {
+          console.log(data);
+          pushText();
+          $('#featured-joke').text(data.value.joke)
+          emptyFirstName();
+        })
+
+      function pushText() {
+        var joke = $('#featured-joke').text()
+        var li = $('<li></li>')
+        li.text(joke)
+        $('#previous-joke').append(li)
+      }
     }
   })
 });
