82ae85f4a690113b23c2af92db4e0c28247e5971
Shirish Dali <designsbydali@gmail.com>
2016-08-10 13:21:07 -0500

    got the landing page to work with an already populated featured joke.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
+  //on page load populate featured-joke
+  var api = 'http://api.icndb.com/jokes/random';
+
+  $.ajax({
+    url: api
+  })
+    .done(function(data) {
+      // console.log(data);
+      $('#featured-joke').html(data.value.joke)
+    })
+
   //onClick the get jokes button
   $('#btn-getjokes').on('click', function() {
-    console.log('it worked');
-    var api = 'http://www.icndb.com/api/';
+    // console.log('it worked');
+    var api = 'http://api.icndb.com/jokes/random';
+    // var next = $('#featured-joke').html(data.value.joke);
 
+    $.ajax({
+      url: api
+    })
+
+      .done(function(data) {
+        console.log(data);
+        $('#featured-joke').html(data.value.joke)
+        // $('#previous-joke').text(next)
+        // $('#weather-img').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
+        // $('#previous').show()
+    })
+    //   .fail(function() {
+    //     alert("error");
+    // });
 
   })
 });
