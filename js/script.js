6d2e33cb8fd2142459d318b0e218432fc6bedc65
Shirish Dali <designsbydali@gmail.com>
2016-08-10 11:39:44 -0500

    onclick fucnction working for the btn-getjokes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  //onClick the get jokes button
+  $('#btn-getjokes').on('click', function() {
+    console.log('it worked');
+    var api = 'http://www.icndb.com/api/';
+
+
+  })
+});
